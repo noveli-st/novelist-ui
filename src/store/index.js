ca8629fe1b58ee14dd06dec202e1409e4fd08e95
Arthur A. Selimov <am35a.piston@gmail.com>
2020-03-02 14:37:25 +0300

    fix for 412/3

--- src/store/index.js
+++ src/store/index.js
@@ -15,7 +15,9 @@ export default new Vuex.Store({
         // Full screen image expander
         expandContainer (state, data) {
             state.isImageExpanded = true
+            document.body.style.paddingRight = window.innerWidth - document.body.clientWidth + 'px'
             document.body.classList.add('overflow-hidden')
+            
             // после структурирования папок на сервере путь будет изменен на правильный!
             state.imageExpanderPath = "http://mobitoon.ru/novelist/images/books/" + data + '/cover.jpg'
         }
