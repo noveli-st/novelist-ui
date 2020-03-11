15122abcc3334ed80c1f4614d353f360d8db7f92
Dmitry Antonyuk <dmitry.antonuke@gmail.com>
2020-03-10 20:47:02 -0500

    Load books for index view

--- src/components/index/Index.vue
+++ src/components/index/Index.vue
@@ -6,19 +6,24 @@
             v-bind:cardainer-subtitle="'Books'"
             v-bind:cardainer-style="'deck'"
             v-bind:cardainer-link="'/books'">
-            <cmp-card v-for="(id, index) in ids.slice(0, 4)" v-bind:key="index" v-bind:book-id=id></cmp-card>
+            <cmp-card v-for="(book, index) in books.slice(0, 4)" v-bind:key="index" v-bind:book="book"></cmp-card>
         </cmp-cardeiner>
     </main>
 </template>
 
 <script>
+    import client from 'api-client'
+
     export default {
         name: 'Index',
         data() {
             return {
-                // template array
-                ids: ['2', '8', '1', '3', '9', '5', '6', '7', '10', '12', '11', '4']
+                books: []
             }
+        },
+        mounted() {
+            client.findBooks()
+                .then(books => (this.books = books.list));
         }
     }
 </script>
