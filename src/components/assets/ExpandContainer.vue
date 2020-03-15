ed706784fff9768f2a9ef3e9a937005818d49329
Arthur A. Selimov <am35a.piston@gmail.com>
2020-03-15 19:27:52 +0300

    I can not find resolve the error

--- src/components/assets/ExpandContainer.vue
+++ src/components/assets/ExpandContainer.vue
@@ -1,16 +1,16 @@
 <template>
-    <div v-if="$store.state.isImageExpanded" v-on:click="closeExpandContainer" class="expand-container cursor-pointer">
+    <div v-if="$store.state.imageExpand.isImageExpanded" v-on:click="closeExpandContainer" class="expand-container cursor-pointer">
         <span class="position-absolute lt-0 p-2 text-white-50">Click or tap to close</span>
-        <img v-bind:src="$store.state.imageExpanderPath" alt="Author: Book title">
+        <img v-bind:src="$store.state.imageExpand.imageExpanderPath" alt="Author: Book title">
     </div>
 </template>
 
 <script>
 	export default {
-		name: 'ExpandContainer',
+        name: 'ExpandContainer',
         methods:{
             closeExpandContainer(){
-                this.$store.state.isImageExpanded = false
+                this.$store.state.imageExpand.isImageExpanded = false
                 // this.$store.state.imageExpanderPath = ''
                 document.body.style.paddingRight = ''
                 document.body.classList.remove('overflow-hidden')
