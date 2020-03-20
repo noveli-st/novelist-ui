693218f865d0ce713086748b39cbed9701cee18b
Dmitry Antonyuk <dmitry.antonuke@gmail.com>
2020-03-19 21:31:51 -0500

    Show user profiles
    Ref #421

--- src/components/profile/Info.vue
+++ src/components/profile/Info.vue
@@ -1,18 +1,18 @@
 <template>
     <div class="container">
-		<div class="row">
-			<div class="col-12 col-lg-8 col-xl-9">
-				info
-			</div>
-			<div class="col-12 col-lg-4 col-xl-3">
-				...
-			</div>
-		</div>
+        <div class="row">
+            <div class="col-12 col-lg-8 col-xl-9">
+                info
+            </div>
+            <div class="col-12 col-lg-4 col-xl-3">
+                ...
+            </div>
+        </div>
     </div>
 </template>
 
 <script>
-	export default {
-		name: 'ProfileInfo'
-	}
+    export default {
+        name: 'ProfileInfo'
+    }
 </script>
