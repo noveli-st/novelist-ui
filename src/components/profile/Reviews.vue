aa9a93465e4cf8d55681fa07285934c705910b3c
Arthur A. Selimov <am35a.piston@gmail.com>
2020-04-05 23:15:13 +0300

    xreate my-books + profile fix

--- src/components/profile/Reviews.vue
+++ src/components/profile/Reviews.vue
@@ -1,6 +1,12 @@
 <template>
     <div class="container">
-		reviews
+		<header class="mb-3">
+            <div class="h2" role="heading" aria-level="2">Reviews</div>
+        </header>
+        <div class="alert alert-info border-0">
+            <font-awesome-icon icon="info-circle" size="lg" class="mr-3"></font-awesome-icon>
+            The section will be soon.
+        </div>
     </div>
 </template>
 
