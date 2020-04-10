a186dfc601a674ef8bbf15ab50f41f9621d400f6
Arthur A. Selimov <am35a.piston@gmail.com>
2020-04-10 22:00:28 +0300

    fixed inline pills add expand button action

--- src/components/content/Books.vue
+++ src/components/content/Books.vue
@@ -10,13 +10,24 @@
             <div class="row">
                 <div class="col-12 col-lg-8 col-xl-9">
                     <div v-bind:class="cardainerStyleClass">
-                        <cmp-card v-for="(book, index) in books.slice(0, 6)" v-bind:key="index" v-bind:book="book"></cmp-card>
+                        <cmp-card v-for="(book, index) in books.slice(0, booksLoadNumb)" v-bind:key="index" v-bind:book="book"></cmp-card>
                     </div>
-                    <button class="btn btn-block btn-outline-primary" type="button"><font-awesome-icon icon="arrow-alt-circle-down" class="mr-2"></font-awesome-icon>Load more books...</button>
+                    <button class="btn btn-block btn-outline-primary" type="button" v-on:click="booksLoadNumb += booksLoadNumb" v-bind:class="{'disabled': booksLoadNumb >= books.length}">
+                        <template v-if="booksLoadNumb < books.length">
+                            <font-awesome-icon icon="arrow-alt-circle-down" class="mr-2"></font-awesome-icon>Load more books...
+                        </template>
+                        <template v-else>
+                            No more books<font-awesome-icon icon="frown" class="ml-2"></font-awesome-icon>
+                        </template>
+                    </button>
                 </div>
                 <div id="asideBlock" class="col-12 col-lg-4 col-xl-3 d-none d-lg-block" role="region" aria-label="Filters for books search">
                     <div class="h3">Filters</div>
                     <hr class="border-top">
+                    <div class="alert alert-info border-0">
+                        <font-awesome-icon icon="info-circle" size="lg" class="mr-3"></font-awesome-icon>
+                        The section will be soon.
+                    </div>
                 </div>
             </div>
         </div>
@@ -31,7 +42,8 @@
         data() {
             return {
                 cardViewStyle: "list",
-                books: []
+                books: [],
+                booksLoadNumb: 6
             }
         },
         computed: {
