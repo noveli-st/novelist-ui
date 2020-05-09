fbb06b08c0916fad6d4ab18faf33e2238dbac77e
Dmitry Antonyuk <dmitry.antonuke@gmail.com>
2020-05-09 14:27:16 -0500

    Add HtmlText component

---
+++ src/components/book/HtmlText.vue
@@ -0,0 +1,21 @@
+<template>
+    <span v-html="convertedText"></span>
+</template>
+<script>
+export default {
+  props: {
+    text: {
+      type: String,
+      default: ""
+    }
+  },
+  computed: {
+    convertedText() {
+      return this.text
+        .replace(/\n|\r|\r\n/g, "<br>")
+        .replace(/(<\/?)div(?![a-z])/gi, (g, start) => `${start}p`)
+        .replace(/(<)(?!(?:\/?(?:p|br|figure|figcaption|img))(?![a-z]))/gi, "&lt;")
+    }
+  }
+}
+</script>
