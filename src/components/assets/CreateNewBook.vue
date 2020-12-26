f1145018af4522279915afe8e648717cd390d3f4
Arthur A. Selimov <am35a.piston@gmail.com>
2020-12-26 18:32:13 +0300

    create ui component

---
+++ src/components/assets/CreateNewBook.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="d-flex mb-3">
+        <button class="btn btn-success col-md-6 mx-auto" type="button"><font-awesome-icon icon="plus" class="mr-2"></font-awesome-icon>Create new book</button>
+        <!-- <div class="col-md-6 px-0 mx-auto d-flex">
+            <input type="text" class="form-control mr-2" placeholder="Type the book title">
+            <button class="btn btn-primary text-nowrap mr-2" type="button"><font-awesome-icon icon="check" class="mr-2"></font-awesome-icon>Create</button>
+            <button class="btn btn-secondary" type="button"><font-awesome-icon icon="times"></font-awesome-icon></button>
+        </div> -->
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'CreateNewBook',
+        data() {
+            return {
+                
+            }
+        },
+        methods: {
+        },
+        computed:{
+        }
+    }
+</script>
